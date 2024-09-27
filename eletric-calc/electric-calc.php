4427c1b8707c60cdcf30fc2226a5343c58e8a7a1
Timohub3005 <Timohoppe07@gmail.com>
2024-09-27 20:59:25 +0200

    feat: added redirects (work in progress)

---
+++ eletric-calc/electric-calc.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ACC-Electric-Calculator</title>
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+<div class="screen"> <!-- Anfang von div Screen -->
+        <div class="sidebar"> <!-- Anfang von div Sidebar -->
+            <button onclick="window.location.href='../index.html'" class="sidebarbutton">Home</button>
+            <button onclick="window.location.href='../pitycalculator/pity-calc.php'" class="sidebarbuttonopen">Pity-calculator</button>
+            <button onclick="window.location.href='electric-calc.php'" class="sidebarbutton">Electric-calcutor</button>
+        </div> <!-- Ende von div Sidebar -->
+
+
+
+    </div> <!-- Ende von div Screen -->
+</body>
+</html>
--- pitycalculator/pity-calc.php
+++ pitycalculator/pity-calc.php
@@ -4,8 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ACC-Pity-Calculator</title>
+    <link rel="stylesheet" href="../style.css">
 </head>
 <body>
-    
+<div class="screen"> <!-- Anfang von div Screen -->
+        <div class="sidebar"> <!-- Anfang von div Sidebar -->
+            <button onclick="window.location.href='../index.html'" class="sidebarbutton">Home</button>
+            <button onclick="window.location.href='pitycalculator/pity-calc.php'" class="sidebarbuttonopen">Pity-calculator</button>
+            <button onclick="window.location.href='../electriccalcutor/electric-calc.php'" class="sidebarbutton">Electric-calcutor</button>
+        </div> <!-- Ende von div Sidebar -->
+
+
+
+    </div> <!-- Ende von div Screen -->
 </body>
 </html>
