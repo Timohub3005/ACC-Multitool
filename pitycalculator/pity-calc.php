81b289e3ecdd5fd5f31e0261fd335748696c7f14
Lynixen <dwishrtt@gmail.com>
2024-09-27 23:43:04 +0200

    feats: 1. Changed around Div Containers in pity-calc.php 2. Commented out the php code as it was unusable/broken 3. Styled the individual containers for calculators
    UNRESOLVED ISSUES:
    Changing div styles in pity-calc.php changes the entire page.

--- pitycalculator/pity-calc.php
+++ pitycalculator/pity-calc.php
@@ -16,38 +16,37 @@
         <button onclick="window.location.href='../electric-calc/electric-calc.php'" class="sidebarbutton">Electric-calculator</button>
     </div> <!-- Ending of div Sidebar -->
 
-
-    <div class="pitycalc1"> <!-- Beginning of the first calculator -->
-        <form action="" method="POST">
+    <div class="pitycalc1"> <!-- Beginning of the first calculator --> <!-- Unused: <?php /* echo isset($_POST['pity']) ? intval($_POST['pity']) : ''; */ ?> -->
+        <form action="pity-calc.php">
             <label for="pity">Pity:</label>
-            <input type="number" id="pity" name="pity" value="<?php echo isset($_POST['pity']) ? intval($_POST['pity']) : ''; ?>" required><br><br>
+            <input type="number" id="pity" name="pity" required><br><br>
 
             <label for="fates">Intertwined Fates:</label>
-            <input type="number" id="fates" name="fates" value="<?php echo isset($_POST['fates']) ? intval($_POST['fates']) : ''; ?>" required><br><br>
+            <input type="number" id="fates" name="fates" required><br><br>
 
             <label for="primogems">Primogems:</label>
-            <input type="number" id="primogems" name="primogems" value="<?php echo isset($_POST['primogems']) ? intval($_POST['primogems']) : ''; ?>" required><br><br>
+            <input type="number" id="primogems" name="primogems" required><br><br>
 
             <input class="buttonrechner" type="submit" value="Submit"><br><br>
         </form>
-    <div>
+    </div>
 
     <div class="wishcalc1">
-        <form action="" method="POST">
+        <form action="pity-calc.php"> <!-- Unused: value="<?php /* echo isset($hardpity) ? $hardpity : ''; */ ?>"  -->
             <label for="hardpity">Wishes until Hardpity:</label>
-            <input type="text" id="hardpity" value="<?php echo isset($hardpity) ? $hardpity : ''; ?>" readonly><br><br>
+            <input type="text" id="hardpity" readonly><br><br>
 
             <label for="softpity">Wishes until Softpity:</label>
-            <input type="text" id="softpity" value="<?php echo isset($softpity) ? $softpity : ''; ?>" readonly><br><br>
+            <input type="text" id="softpity" readonly><br><br>
 
             <label for="availablewishes">Wishes availabe:</label>
-            <input type="text" id="availablewishes" value="<?php echo isset($fates) ? $fates : ''; ?>" readonly><br><br>
+            <input type="text" id="availablewishes" readonly><br><br>
 
             <label for="hardwish">Wishes until hardpity (w/ counting the availabe wishes):</label>
-            <input type="text" id="hardwish" value="<?php echo isset($hardwish) && $hardwish > 0 ? $hardwish : 0; ?>" readonly><br><br>
+            <input type="text" id="hardwish"  readonly><br><br>
 
             <label for="softwish">Wishes until softpity (w/ counting the availabe wishes)</label>
-            <input type="text" id="softwish" value="<?php echo isset($softwish) && $softwish > 0 ? $softwish : 0; ?>" readonly><br><br>
+            <input type="text" id="softwish" readonly><br><br>
         </form>
  <!-- Ending of the first calculator -->
     </div>
