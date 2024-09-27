7dddf4877f9017358d5ea7d1f0c522d87924ccf5
Lynixen <dwishrtt@gmail.com>
2024-09-27 23:25:46 +0200

    feats: 1. Changed around Div Containers in pity-calc.php 2. Commented out the php code as it was unusable/broken 3. Styled the individual containers for calculators
    UNRESOLVED ISSUES:
    Changing div styles in pity-calc.php changes the entire page.

--- pitycalculator/pity-calc.php
+++ pitycalculator/pity-calc.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ACC-Pity-Calculator</title>
+    <link rel="stylesheet" href="stylepity.css">
     <link rel="stylesheet" href="../style.css">
 </head>
 <body>
@@ -17,19 +18,42 @@
 
 
     <div class="pitycalc1"> <!-- Beginning of the first calculator -->
-    <form action="" method="POST">
-        <label for="pity">Pity:</label>
-        <input type="number" id="pity" name="pity" value="<?php echo isset($_POST['pity']) ? intval($_POST['pity']) : ''; ?>" required><br><br>
+        <form action="" method="POST">
+            <label for="pity">Pity:</label>
+            <input type="number" id="pity" name="pity" value="<?php echo isset($_POST['pity']) ? intval($_POST['pity']) : ''; ?>" required><br><br>
 
-        <label for="fates">Intertwined Fates:</label>
-        <input type="number" id="fates" name="fates" value="<?php echo isset($_POST['fates']) ? intval($_POST['fates']) : ''; ?>" required><br><br>
+            <label for="fates">Intertwined Fates:</label>
+            <input type="number" id="fates" name="fates" value="<?php echo isset($_POST['fates']) ? intval($_POST['fates']) : ''; ?>" required><br><br>
 
-        <label for="primogems">Primogems:</label>
-        <input type="number" id="primogems" name="primogems" value="<?php echo isset($_POST['primogems']) ? intval($_POST['primogems']) : ''; ?>" required><br><br>
+            <label for="primogems">Primogems:</label>
+            <input type="number" id="primogems" name="primogems" value="<?php echo isset($_POST['primogems']) ? intval($_POST['primogems']) : ''; ?>" required><br><br>
 
-        <input class="buttonrechner" type="submit" value="Submit"><br><br>
+            <input class="buttonrechner" type="submit" value="Submit"><br><br>
+        </form>
+    <div>
 
-        <?php
+    <div class="wishcalc1">
+        <form action="" method="POST">
+            <label for="hardpity">Wishes until Hardpity:</label>
+            <input type="text" id="hardpity" value="<?php echo isset($hardpity) ? $hardpity : ''; ?>" readonly><br><br>
+
+            <label for="softpity">Wishes until Softpity:</label>
+            <input type="text" id="softpity" value="<?php echo isset($softpity) ? $softpity : ''; ?>" readonly><br><br>
+
+            <label for="availablewishes">Wishes availabe:</label>
+            <input type="text" id="availablewishes" value="<?php echo isset($fates) ? $fates : ''; ?>" readonly><br><br>
+
+            <label for="hardwish">Wishes until hardpity (w/ counting the availabe wishes):</label>
+            <input type="text" id="hardwish" value="<?php echo isset($hardwish) && $hardwish > 0 ? $hardwish : 0; ?>" readonly><br><br>
+
+            <label for="softwish">Wishes until softpity (w/ counting the availabe wishes)</label>
+            <input type="text" id="softwish" value="<?php echo isset($softwish) && $softwish > 0 ? $softwish : 0; ?>" readonly><br><br>
+        </form>
+ <!-- Ending of the first calculator -->
+    </div>
+</div>
+
+        <?php /*
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $pity = isset($_POST['pity']) ? intval($_POST['pity']) : 0;
             $primogems = isset($_POST['primogems']) ? intval($_POST['primogems']) : 0;
@@ -41,24 +65,7 @@
             $hardwish = 90 - $pity - $fates;
             $softwish = 70 - $pity - $fates;
         }
-        ?>
 
-        <label for="hardpity">Wishes until Hardpity:</label>
-        <input type="text" id="hardpity" value="<?php echo isset($hardpity) ? $hardpity : ''; ?>" readonly><br><br>
-
-        <label for="softpity">Wishes until Softpity:</label>
-        <input type="text" id="softpity" value="<?php echo isset($softpity) ? $softpity : ''; ?>" readonly><br><br>
-
-        <label for="availablewishes">Wishes availabe:</label>
-        <input type="text" id="availablewishes" value="<?php echo isset($fates) ? $fates : ''; ?>" readonly><br><br>
-
-        <label for="hardwish">Wishes until hardpity (w/ counting the availabe wishes):</label>
-        <input type="text" id="hardwish" value="<?php echo isset($hardwish) && $hardwish > 0 ? $hardwish : 0; ?>" readonly><br><br>
-
-        <label for="softwish">Wishes until softpity (w/ counting the availabe wishes)</label>
-        <input type="text" id="softwish" value="<?php echo isset($softwish) && $softwish > 0 ? $softwish : 0; ?>" readonly><br><br>
-
-        <?php
         if (isset($softwish) && $softwish <= 0) {
             $softwish = abs($softwish);
             echo "<p>Softpity kann mit den verfügbaren Wishes erreicht werden, übrig nach Softpity: $softwish</p>";
@@ -67,10 +74,8 @@
         if (isset($hardwish) && $hardwish <= 0) {
             $hardwish = abs($hardwish);
             echo "<p>Hardpity kann mit den verfügbaren Wishes erreicht werden, übrig nach Hardpity: $hardwish</p>";
-        }
-        ?> <!-- Ending of the first calculator -->
-    </form>
-</div>
+        } */
+        ?>
 
 </body>
 </html>
